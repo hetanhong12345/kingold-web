cdc81c546219b2eec1c4160b6121da9982e63d44
rruanruan <rruanruan@163.com>
2018-03-08 14:46:11 +0800

    edit

--- src/containers/Center.vue
+++ src/containers/Center.vue
@@ -1,12 +1,36 @@
 <template>
     <div class="center">
-center
+<div class="content" flex="dir:top main:center cross:center">
+   <p class="total">7000,00</p>
+    <p class="info">可用余额（元）</p>
+    <button class="btn">充值</button>
+</div>
+        <div class="content2" flex>
+           <div>
+               <p>
+                   10000,00
+               </p>
+               <p>总资产（元）</p>
+           </div>
+            <div>
+                <p>
+                    10000,00
+                </p>
+                <p>总资产（元）</p>
+            </div>
+            <div>
+                <p>
+                    10000,00
+                </p>
+                <p>总资产（元）</p>
+            </div>
+        </div>
     </div>
 
 </template>
 
 <script>
-
+import '../less/center.less';
     export default {
         name: 'center',
         data() {
